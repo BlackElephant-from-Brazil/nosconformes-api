dcf7a7788abf64b4a5b8039582b8a74c5134e097
Guilherme Sartori <gui.sartori96@gmail.com>
2023-03-06 08:10:31 -0300

    feat(tests): update users tests

--- src/modules/companies/controllers/update-company.controller.ts
+++ src/modules/companies/controllers/update-company.controller.ts
@@ -12,7 +12,6 @@ import { JwtAuthGuard } from '../../auth/guards/jwt-auth.guard';
 import { UpdateAuditorsReqDTO } from '../dtos/req/update-auditors.req.dto';
 import { UpdateCompanyReqDTO } from '../dtos/req/update-company.req.dto';
 import { UpdateManagerReqDTO } from '../dtos/req/update-manager.req.dto';
-import { UpdateAuditorsFromCompanyService } from '../services/update-auditors-from-company.service';
 import { UpdateCompanyDataService } from '../services/update-company-data.service';
 import { UpdateManagerDataService } from '../services/update-manager-data.service';
 
@@ -21,7 +20,6 @@ export class UpdateCompanyController {
 	constructor(
 		private readonly updateCompanyDataService: UpdateCompanyDataService,
 		private readonly updateManagerDataService: UpdateManagerDataService,
-		private readonly updateAuditorsFromCompanyService: UpdateAuditorsFromCompanyService,
 	) {}
 	@UseGuards(JwtAuthGuard)
 	@Put('/company/:id')
@@ -51,21 +49,5 @@ export class UpdateCompanyController {
 		);
 
 		res.json(company).status(HttpStatus.OK);
-	}
-
-	@UseGuards(JwtAuthGuard)
-	@Put('/auditors/:id')
-	async updateAuditors(
-		@Body() { auditors }: UpdateAuditorsReqDTO,
-		@Param('id') companyId: string,
-		@Res() res: Response,
-	) {
-		const availableAuditors =
-			await this.updateAuditorsFromCompanyService.execute(
-				companyId,
-				auditors,
-			);
-
-		res.json(availableAuditors).status(HttpStatus.OK);
 	}
 }
--- src/modules/companies/services/index.ts
+++ src/modules/companies/services/index.ts
@@ -1,7 +1,6 @@
 import { CreateCompanyService } from './create-company.service';
 import { FindAllCompaniesService } from './find-all-companies.service';
 import { FindCompanyByIdService } from './find-company-by-id.service';
-import { UpdateAuditorsFromCompanyService } from './update-auditors-from-company.service';
 import { UpdateCompanyDataService } from './update-company-data.service';
 import { UpdateManagerDataService } from './update-manager-data.service';
 
@@ -9,7 +8,6 @@ export const companiesServices = [
 	CreateCompanyService,
 	FindAllCompaniesService,
 	FindCompanyByIdService,
-	UpdateAuditorsFromCompanyService,
 	UpdateCompanyDataService,
 	UpdateManagerDataService,
 ];
